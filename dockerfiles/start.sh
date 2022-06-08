a2955d5803f1194268cb907ebd209cbb14f7dab6
Feng Shang <fengshang@didichuxing.com>
2022-06-08 13:00:32 +0800

    ci: remove didi dir

--- dockerfiles/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# start service
-cd /workspace
-JAR_FILE=`ls | grep .jar$`
-$JAVA_HOME/bin/java -jar -Dspring.profiles.active=$env $JAR_FILE
--- elevate/push-docker.sh
+++
@@ -1,6 +0,0 @@
-./mvnw package
-
-docker build -t hub.xiaojukeji.com/dengfeige/feature-probe/api .
-docker push hub.xiaojukeji.com/dengfeige/feature-probe/api
-
-echo "Push docker image successful."
