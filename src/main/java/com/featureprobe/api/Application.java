bc6513067a21a6794a83c00e41703daccf817bb1
jianggang <gangb.info@gmail.com>
2022-10-20 10:39:28 +0800

    feat: turn off sql printing

--- src/main/java/com/featureprobe/api/Application.java
+++ src/main/java/com/featureprobe/api/Application.java
@@ -2,10 +2,12 @@ package com.featureprobe.api;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+@Slf4j
 @EnableCaching
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Feature Probe API", version = "1.0", description = "Feature Probe API"))
@@ -13,6 +15,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("FeatureProbe API startup completed .");
     }
 
 }
